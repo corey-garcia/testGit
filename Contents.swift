41a821b95da4910402ad929d52560c8dc1b28e0c
corey.w.garcia <corey.w.garcia@outlook.com>
2019-06-17 12:22:41 -0400

    Initial Save

---
+++ Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+
+//I created a variable called placeholder, and I put the String (Red Thing) inside placeHolder
+var placeHolder = "Hi! I have not had nearly enough coffee this morning"
+
+//function; and your arguments go inside the parentheses
+print(placeHolder)
