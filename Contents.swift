322079b1a44f6b2d610768cd47ec0d7079032d3c
corey.w.garcia <corey.w.garcia@outlook.com>
2019-06-17 16:25:41 -0400

    Initial Save

--- Contents.swift
+++ Contents.swift
@@ -1,8 +1,91 @@
-import UIKit
+//import UIKit
+//
+//
+////I created a variable called placeholder, and I put the String (Red Thing) inside placeHolder
+////var placeHolder = "Hi! I have not had nearly enough coffee this morning"
+//
+////function; and your arguments go inside the parentheses
+////print(placeHolder)
+//
+////print("Hi Brandi")
+////print("Hi Corey")
+////print("Hi Sara")
+//
+////Idea: Design our code to be able to take anything we want to say, and put what we want to say out on the console. Our first function:
+////camelcase: spaces are bad, so when we name something, make it one word.
+////first word not capitalized, everyword after: capitalized
+////insde (): your parameters: what goes into the function'
+//// : -> "of type"
+//
+//
+////we made our function here
+//func outputName(userInput: String) {
+//    //our function goes here
+//    print("\(userInput)")
+//}
+//
+////test our function
+//var test = "It is raining, and I thought Miami would be sunny."
+//outputName(userInput: test)
+//
+//
+//
+//
+
+//var favoriteFood = "Taco Bell"
+//
+//if favoriteFood == "Chipotle" {
+//    print("You choose")
+//} else if favoriteFood == "Starbucks" {
+//    print("My fave food is not Starbucks")
+//} else if favoriteFood == "BurgerKing" {
+//    print("My fave food is indeed Burger King")
+//} else {
+//    print("My favorite food was \(favoriteFood)")
+//}
 
 
-//I created a variable called placeholder, and I put the String (Red Thing) inside placeHolder
-var placeHolder = "Hi! I have not had nearly enough coffee this morning"
+//Code Challenge:
+//Write a FUNCTION called gradeChecker that takes in a number called testscore, and checks prints out a letter grade:
+//A: 90 - 100; 95 -> A
+//B: 80 - 89; 86 -> B
+//C: 70 - 79; 72 -> C
+//D: 60 - 69; 61 -> D
+//F: anything below a 60 -> F
+//how do you represent numbers? Which data type represents numbers?
+//extension: make your function print out the original score, along with the letter grade
 
-//function; and your arguments go inside the parentheses
-print(placeHolder)
+//collections: a way we collect data
+var name1 = "Shannon"
+var name2 = "Jennifer"
+var name3 = "Amber"
+
+//declare an array
+var myNames = [String]()
+
+//we added a bunch of stuff to an array
+myNames.append(name1)
+myNames.append(name2)
+myNames.append(name3)
+//print(myNames)
+
+//we will pull out data from an array
+myNames[2]
+
+//Code Challenge: Make an array that holds 6 of your favorite
+//hobbies, in decreasing order; Pos. 0: Most Important; Pos. 5 would be the least important
+//Share your most important hobby, with the person next to you
+
+var myHobbies = [String]()
+
+myHobbies.append("KickBoxing")
+myHobbies.append("Painting")
+myHobbies.append("Jetskiing")
+myHobbies.append("Girlscouting")
+myHobbies.append("Dancing")
+myHobbies.append("sleep")
+
+
+
+
+
